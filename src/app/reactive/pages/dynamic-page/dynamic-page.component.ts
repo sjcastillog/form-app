5561faf7e5335172e165e21eb1824f2e19c01926
Joao Castillo <joaocastillo91@gmail.com>
2023-06-20 17:28:02 -0500

    first commit today 20-06-2023

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,5 +7,88 @@ import { Component } from '@angular/core';
   ]
 })
 export class DynamicPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  });
+
+
+  public newFavorite: FormControl = new FormControl('',[])
+
+  constructor( private fb:FormBuilder){}
+
+
+
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField( field:string ):boolean | null{
+    return this.myForm.controls[field].errors 
+        && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field:string ):string | null{
+
+    if(!this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {} ;
+
+    for (const key of Object.keys(errors)) {
+
+      switch( key){
+        case 'required': 
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracteres.`
+      }
+      
+    }
+
+    return null;
+  }
+
+
+  isValidFieldInArray(formArray:FormArray, index:number ):boolean | null{
+    return formArray.controls[index].errors 
+        && formArray.controls[index].touched;
+  }
+
+  onAddToFavorites():void{
+    
+    if( this.newFavorite.invalid ) return;
+
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push( 
+      this.fb.control( newGame, Validators.required)
+    );
+
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite( index:number ):void{
+    this.favoriteGames.removeAt(index);
+  }
+
+
+  onSubmit(e:Event):void{
+    e.preventDefault();
+    if( this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log( this.myForm.value );
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([])
+    this.myForm.reset();
+
+  }
 
 }
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm:FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications:[ true, Validators.required],
+    termsAndConditions:[ false, Validators.requiredTrue],
+  });
+
+  public person = {
+    gender:'F',
+    wantNotifications:false,
+  }
+
+  constructor( private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person);
+  }
+
+
+  onSave(){
+    if( this.myForm.invalid){
+      this.myForm.markAllAsTouched;
+      return;
+    }
+
+    console.log(this.myForm.value);
+    const { termsAndConditions, ...newPerson }  = this.myForm.value;
+    this.person = newPerson;
+  }
+
+  isValidField( field:string ):boolean | null{
+    return this.myForm.controls[field].errors 
+        && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field:string ):string | null{
+
+    if(!this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {} ;
+
+    for (const key of Object.keys(errors)) {
+
+      switch( key){
+        case 'required': 
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracteres.`
+      }
+      
+    }
+
+    return null;
+  }
+
 
 }
